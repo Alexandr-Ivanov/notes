0ac01d981436fa85a20ceb13d2bd22596bc452d5
Alexandr-Ivanov <ivanov2000@bk.ru>
2026-09-01 18:14:01 +0700

    Created MenuFormer class

---
+++ src/main/java/ru/ivanov/cft_testcase/notes/MenuFormer.java
@@ -0,0 +1,109 @@
+/**
+ *
+ */
+package ru.ivanov.cft_testcase.notes;
+
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
+
+import ru.ivanov.cft_testcase.notes.views.StringDialog;
+
+/**
+ * @author a.ivanov
+ *
+ */
+public class MenuFormer {
+
+    public static void addMenuItems(Shell shell, Table table, Domain domain, Menu menuBar) {
+        MenuItem addNoteItem = new MenuItem(menuBar, SWT.PUSH);
+        addNoteItem.setText(ADD_NOTE);
+        addNoteItem.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                StringDialog dialog = new StringDialog(shell, ADD_NOTE);
+                String content = dialog.open("");
+
+                if (null != content && !content.isEmpty()) {
+                    domain.addNote(content);
+                    refreshTable(table, domain);
+                }
+            }
+        });
+
+        MenuItem editNoteItem = new MenuItem(menuBar, SWT.PUSH);
+        editNoteItem.setText(EDIT_NOTE);
+        editNoteItem.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                TableItem[] selection = table.getSelection();
+
+                if (0 < selection.length) {
+                    TableItem item = selection[0];
+                    System.out.println(item.getText(0));
+                    StringDialog dialog = new StringDialog(shell, EDIT_NOTE);
+                    String content = dialog.open(item.getText(1));
+
+                    if (null != content && !content.isEmpty()) {
+                        Note note = domain.getNote(Long.parseLong(item.getText(0)));
+                        note.setContent(content);
+                        domain.updateNote(note);
+                        refreshTable(table, domain);
+                    }
+                }
+            }
+        });
+
+        MenuItem deleteNoteItem = new MenuItem(menuBar, SWT.PUSH);
+        deleteNoteItem.setText(DELETE_NOTE);
+        deleteNoteItem.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                TableItem[] selection = table.getSelection();
+
+                if (0 < selection.length) {
+                    TableItem item = selection[0];
+                    System.out.println(item.getText(0));
+                    MessageBox box = new MessageBox(shell, SWT.OK | SWT.CANCEL | SWT.ICON_QUESTION | SWT.APPLICATION_MODAL);
+                    box.setMessage(DELETE_NOTE + ": " + item.getText(1));
+                    int result = box.open();
+                    System.out.println(result);
+
+                    if (SWT.OK == result) {
+                        domain.deleteNote(Long.parseLong(item.getText(0)));
+                        refreshTable(table, domain);
+                    }
+                }
+            }
+        });
+    }
+
+    protected static void refreshTable(Table table, Domain domain) {
+        table.setLinesVisible(false);
+        table.removeAll();
+        fillTable(table, domain);
+        table.setLinesVisible(true);
+    }
+
+    private static void fillTable(Table table, Domain domain) {
+        List<Note> notes = domain.getAllNotes();
+
+        for (Note note : notes) {
+            TableItem item = new TableItem(table, SWT.NONE);
+            item.setText(new String[] { Long.toString(note.getId()), note.getContent() });
+        }
+    }
+
+    private static final String TITLE = "Notes";
+    private static final String ADD_NOTE = " добавить запись";
+    private static final String EDIT_NOTE = "изменить запись";
+    private static final String DELETE_NOTE = "удалить запись";
+}
--- src/main/java/ru/ivanov/cft_testcase/notes/NotesApp.java
+++ src/main/java/ru/ivanov/cft_testcase/notes/NotesApp.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package ru.ivanov.cft_testcase.notes;
 
@@ -10,17 +10,12 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.MenuItem;
-import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.*;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import ru.ivanov.cft_testcase.notes.Domain;
+import ru.ivanov.cft_testcase.notes.MenuFormer;
 import ru.ivanov.cft_testcase.notes.views.StringDialog;
 
 /**
@@ -29,146 +24,84 @@ import ru.ivanov.cft_testcase.notes.views.StringDialog;
  */
 public class NotesApp {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		try (SessionFactory sessionFactory = initSessionFactory()) {
-			Domain domain = new Domain(sessionFactory);
-			Display display = new Display();
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        try (SessionFactory sessionFactory = initSessionFactory()) {
+            Domain domain = new Domain(sessionFactory);
+            Display display = new Display();
 
-			Shell shell = newShell(display, domain);
+            Shell shell = newShell(display, domain);
 
-			shell.open();
-			while (!shell.isDisposed()) {
-				if (!display.readAndDispatch()) {
-					display.sleep();
-				}
-			}
+            shell.open();
+            while (!shell.isDisposed()) {
+                if (!display.readAndDispatch()) {
+                    display.sleep();
+                }
+            }
 
-			display.dispose();
-		}
-	}
+            display.dispose();
+        }
+    }
 
-	private static Shell newShell(Display display, Domain domain) {
-		Shell shell = new Shell(display);
-		shell.setText(TITLE);
-		FillLayout layout = new FillLayout();
-		shell.setLayout(layout);
+    private static Shell newShell(Display display, Domain domain) {
+        Shell shell = new Shell(display);
+        shell.setText(TITLE);
+        FillLayout layout = new FillLayout();
+        shell.setLayout(layout);
 
-		Table table = newTable(domain, shell);
-		table.setLinesVisible(true);
-		formMenu(shell, table, domain);
-		return shell;
-	}
+        Table table = newTable(domain, shell);
+        table.setLinesVisible(true);
+        formMenu(shell, table, domain);
+        return shell;
+    }
 
-	private static void formMenu(Shell shell, Table table, Domain domain) {
-		Menu menuBar = new Menu(shell, SWT.BAR);
-		MenuItem addNoteItem = new MenuItem(menuBar, SWT.PUSH);
-		addNoteItem.setText(ADD_NOTE);
-		addNoteItem.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				StringDialog dialog = new StringDialog(shell, ADD_NOTE);
-				String content = dialog.open("");
-				
-				if (null != content && !content.isEmpty()) {
-					domain.addNote(content);
-					refreshTable(table, domain);
-				}
-			}
-		});
-		
-		MenuItem editNoteItem = new MenuItem(menuBar, SWT.PUSH);
-		editNoteItem.setText(EDIT_NOTE);
-		editNoteItem.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				TableItem[] selection = table.getSelection();
-				
-				if (0 < selection.length) {
-					TableItem item = selection[0];
-					System.out.println(item.getText(0));
-					StringDialog dialog = new StringDialog(shell, EDIT_NOTE);
-					String content = dialog.open(item.getText(1));
-					
-					if (null != content && !content.isEmpty()) {
-						Note note = domain.getNote(Long.parseLong(item.getText(0)));
-						note.setContent(content);
-						domain.updateNote(note);
-						refreshTable(table, domain);
-					}
-				}				
-			}
-		});
-		
-		MenuItem deleteNoteItem = new MenuItem(menuBar, SWT.PUSH);
-		deleteNoteItem.setText(DELETE_NOTE);
-		deleteNoteItem.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				TableItem[] selection = table.getSelection();
-				
-				if (0 < selection.length) {
-					TableItem item = selection[0];
-					System.out.println(item.getText(0));
-					MessageBox box = new MessageBox(shell, SWT.OK | SWT.CANCEL | SWT.ICON_QUESTION | SWT.APPLICATION_MODAL);
-					box.setMessage(DELETE_NOTE + ": " + item.getText(1));
-					int result = box.open();
-					System.out.println(result);
-					
-					if (SWT.OK == result) {
-						domain.deleteNote(Long.parseLong(item.getText(0)));
-						refreshTable(table, domain);
-					}
-				}
-			}
-		});
-		
-		shell.setMenuBar(menuBar);
-	}
+    private static void formMenu(Shell shell, Table table, Domain domain) {
+        Menu menuBar = new Menu(shell, SWT.BAR);
+        MenuFormer.addMenuItems(shell, table, domain, menuBar);
+        shell.setMenuBar(menuBar);
+    }
 
-	protected static void refreshTable(Table table, Domain domain) {
-		table.setLinesVisible(false);
-		table.removeAll();
-		fillTable(table, domain);
-		table.setLinesVisible(true);
-	}
+    protected static void refreshTable(Table table, Domain domain) {
+        table.setLinesVisible(false);
+        table.removeAll();
+        fillTable(table, domain);
+        table.setLinesVisible(true);
+    }
 
-	/**
-	 * @param domain
-	 * @param shell
-	 * @return
-	 */
-	private static Table newTable(Domain domain, Shell shell) {
-		Table table = new Table(shell, SWT.BORDER | SWT.SINGLE);
-		GridData data = new GridData(SWT.RIGHT, SWT.LEFT, true, true);
-		data.heightHint = 200;
-		table.setLayoutData(data);
-		TableColumn tableColumn = new TableColumn(table, SWT.RIGHT);
-		tableColumn.setWidth(50);
-		TableColumn tableColumn2 = new TableColumn(table, SWT.LEFT);
-		tableColumn2.setWidth(500);
-		fillTable(table, domain);
-		return table;
-	}
+    /**
+     * @param domain
+     * @param shell
+     * @return
+     */
+    private static Table newTable(Domain domain, Shell shell) {
+        Table table = new Table(shell, SWT.BORDER | SWT.SINGLE);
+        GridData data = new GridData(SWT.RIGHT, SWT.LEFT, true, true);
+        data.heightHint = 200;
+        table.setLayoutData(data);
+        TableColumn tableColumn = new TableColumn(table, SWT.RIGHT);
+        tableColumn.setWidth(50);
+        TableColumn tableColumn2 = new TableColumn(table, SWT.LEFT);
+        tableColumn2.setWidth(500);
+        fillTable(table, domain);
+        return table;
+    }
 
-	private static void fillTable(Table table, Domain domain) {
-		List<Note> notes = domain.getAllNotes();
+    private static void fillTable(Table table, Domain domain) {
+        List<Note> notes = domain.getAllNotes();
 
-		for (Note note : notes) {
-			TableItem item = new TableItem(table, SWT.NONE);
-			item.setText(new String[] { Long.toString(note.getId()), note.getContent() });
-		}
-	}
+        for (Note note : notes) {
+            TableItem item = new TableItem(table, SWT.NONE);
+            item.setText(new String[] { Long.toString(note.getId()), note.getContent() });
+        }
+    }
 
-	private static SessionFactory initSessionFactory() {
-		Configuration configuration = new Configuration().configure();
+    private static SessionFactory initSessionFactory() {
+        Configuration configuration = new Configuration().configure();
         return configuration.buildSessionFactory();
-	}
+    }
 
-	private static final String TITLE = "Notes";
-	private static final String ADD_NOTE = "добавить запись";
-	private static final String EDIT_NOTE = "изменить запись";
-	private static final String DELETE_NOTE = "удалить запись";
+    private static final String TITLE = "Notes";
+    // Moved to MenuFormer: ADD_NOTE, EDIT_NOTE, DELETE_NOTE
 }
