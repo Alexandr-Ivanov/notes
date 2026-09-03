05a4b40b1d5f564c136a753c9d91c851000a9d6c
Alexandr-Ivanov <ivanov2000@bk.ru>
2026-09-03 13:29:05 +0700

    Created Config class

---
+++ src/main/java/ru/ivanov/cft_testcase/notes/Config.java
@@ -0,0 +1,57 @@
+package ru.ivanov.cft_testcase.notes;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Config {
+    private static final String TITLE = "Notes";
+
+
+    public static SessionFactory initSessionFactory() {
+        var configuration = new Configuration().configure();
+        return configuration.buildSessionFactory();
+    }
+
+    public static Shell newShell(Display display, Domain domain) {
+        var shell = createShell(display);
+        var table = newTable(shell);
+        var controller = new NotesController(table, domain);
+        formMenu(shell, table, controller);
+        controller.fillTable();
+        return shell;
+    }
+
+    private static Shell createShell(Display display) {
+        var shell = new Shell(display);
+        shell.setText(TITLE);
+        var layout = new FillLayout();
+        shell.setLayout(layout);
+        return shell;
+    }
+
+    /**
+     * @param shell
+     * @return
+     */
+    private static Table newTable(Shell shell) {
+        var table = new Table(shell, SWT.BORDER | SWT.SINGLE);
+        var data = new GridData(SWT.RIGHT, SWT.LEFT, true, true);
+        data.heightHint = 200;
+        table.setLayoutData(data);
+        var tableColumn = new TableColumn(table, SWT.RIGHT);
+        tableColumn.setWidth(50);
+        var tableColumn2 = new TableColumn(table, SWT.LEFT);
+        tableColumn2.setWidth(500);
+        return table;
+    }
+
+    private static void formMenu(Shell shell, Table table, NotesController controller) {
+        var menuBar = new Menu(shell, SWT.BAR);
+        MenuFormer.addMenuItems(shell, table, menuBar, controller);
+        shell.setMenuBar(menuBar);
+    }
+}
--- src/main/java/ru/ivanov/cft_testcase/notes/NotesApp.java
+++ src/main/java/ru/ivanov/cft_testcase/notes/NotesApp.java
@@ -3,12 +3,7 @@
  */
 package ru.ivanov.cft_testcase.notes;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.*;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
 /**
  * @author a.ivanov
@@ -20,9 +15,9 @@ public class NotesApp {
      * @param args
      */
     public static void main(String[] args) {
-        try (var sessionFactory = initSessionFactory()) {
+        try (var sessionFactory = Config.initSessionFactory()) {
             var display = new Display();
-            var shell = newShell(display, new Domain(sessionFactory));
+            var shell = Config.newShell(display, new Domain(sessionFactory));
             shell.open();
 
             while (!shell.isDisposed()) {
@@ -35,49 +30,4 @@ public class NotesApp {
         }
     }
 
-    private static Shell newShell(Display display, Domain domain) {
-        var shell = createShell(display);
-        var table = newTable(shell);
-        var controller = new NotesController(table, domain);
-        formMenu(shell, table, controller);
-        controller.fillTable();
-        return shell;
-    }
-
-    private static Shell createShell(Display display) {
-        var shell = new Shell(display);
-        shell.setText(TITLE);
-        var layout = new FillLayout();
-        shell.setLayout(layout);
-        return shell;
-    }
-
-    private static void formMenu(Shell shell, Table table, NotesController controller) {
-        var menuBar = new Menu(shell, SWT.BAR);
-        MenuFormer.addMenuItems(shell, table, menuBar, controller);
-        shell.setMenuBar(menuBar);
-    }
-
-    /**
-     * @param shell
-     * @return
-     */
-    private static Table newTable(Shell shell) {
-        var table = new Table(shell, SWT.BORDER | SWT.SINGLE);
-        var data = new GridData(SWT.RIGHT, SWT.LEFT, true, true);
-        data.heightHint = 200;
-        table.setLayoutData(data);
-        var tableColumn = new TableColumn(table, SWT.RIGHT);
-        tableColumn.setWidth(50);
-        var tableColumn2 = new TableColumn(table, SWT.LEFT);
-        tableColumn2.setWidth(500);
-        return table;
-    }
-
-    private static SessionFactory initSessionFactory() {
-        var configuration = new Configuration().configure();
-        return configuration.buildSessionFactory();
-    }
-
-    private static final String TITLE = "Notes";
 }
